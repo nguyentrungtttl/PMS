39ccba6bae7af6fe927e6efce38a72f6c863aa1d
Khánh Nhi <nhi.tuong25@student.passerellesnumeriques.org>
2024-07-29 10:54:18 +0700

    add navigaste

--- obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae432e5c78043e6802a966e5949c377f1935e99c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec1f68c43e6d167ee038431d99ff9ee43c50732")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
