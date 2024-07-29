3f842a426a27c56d71bf31f32d272e356e86d92d
Khánh Nhi <nhi.tuong25@student.passerellesnumeriques.org>
2024-07-29 09:38:34 +0700

    calendar

--- obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325da7587293409c68181005a8a4867c8e748c40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+690ca84eec550b78577c3cdef5d298f80eea252a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
