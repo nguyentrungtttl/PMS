bfc0920c7cd6818c611b2f14b2ab4c2513243b77
User <user@MacBook-Air-cua-User.local>
2024-07-29 09:05:39 +0700

    oke

--- obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/HotelManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd773a40558cbdbd209408488ca9556a4dbde1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a0b6c763e568f51693cfe8169e0be2ae5220b60")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
